d629983a86e2c25bf55aee1dffb9d3e8bd0f087f
Kiryl Bokach <kiryl@bokach.com>
2020-04-04 22:46:45 -0400

    - Add solution file - Add business logic project - Add board abstraction - Add player abstraction

---
+++ src/TicTacToe/CyrisOpen.TicTacToe.Logic/Abstractions/IBoard.cs
@@ -0,0 +1,18 @@
+﻿namespace CyrisOpen.TicTacToe.Logic.Abstractions
+{
+    /// <summary>
+    /// Generic abstraction for the game board
+    /// </summary>
+    public interface IBoard<TGameState>
+    {
+        /// <summary>
+        /// Resets board to its initial state
+        /// </summary>
+        void Reset();
+
+        /// <summary>
+        /// Gets or sets the current state of the game
+        /// </summary>
+        TGameState State { get; set; }
+    }
+}
---
+++ src/TicTacToe/CyrisOpen.TicTacToe.Logic/Abstractions/IPlayer.cs
@@ -0,0 +1,18 @@
+﻿namespace CyrisOpen.TicTacToe.Logic.Abstractions
+{
+    /// <summary>
+    /// Generic abstraction for a board game player
+    /// </summary>
+    interface IPlayer<TGameState>
+    {
+        /// <summary>
+        /// Makes a move for given state of the board
+        /// </summary>
+        TGameState MakeMove(TGameState boardState);
+
+        /// <summary>
+        /// Analyzes the outcome of a finished game
+        /// </summary>
+        void AnalyzeOutcome(TGameState boardState);
+    }
+}
